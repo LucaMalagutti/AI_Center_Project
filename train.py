47fbad48f6bb31e78cfb1f923ba5e00d665587e7
alessandroruzzi <alrxruzzi98@gmail.com>
2022-04-25 17:21:33 +0200

    setup for tucker training, with wandb

---
+++ train.py
@@ -0,0 +1,41 @@
+#TODO Setup parameters in config files
+#TODO Setup ReadME
+
+from pykeen.pipeline import pipeline
+
+pipeline_result = pipeline(
+     dataset='wn18rr',
+     model='TuckER',
+     loss='bcewithlogits',
+     training_loop='sLCWA', # maybe needs to be changed
+     negative_sampler='basic', # maybe needs to be changed
+     training_kwargs=dict(
+       num_epochs=500,
+       batch_size = 128,
+       checkpoint_name='my_checkpoint.pt',
+       checkpoint_frequency=50,
+       checkpoint_directory='checkpoints',
+       label_smoothing = 0.1
+    ),
+     model_kwargs=dict(
+        embedding_dim = 200 ,
+        relation_dim = 30,
+        dropout_0 = 0.2,
+        dropout_1 = 0.2,
+        dropout_2 = 0.3,
+     ),
+     optimizer = 'Adam',
+     optimizer_kwargs = dict(
+         lr = 0.003,
+     ),
+     lr_scheduler='ExponentialLR',
+     lr_scheduler_kwargs=dict(
+         gamma=1.0,
+     ),
+     result_tracker='wandb',
+     result_tracker_kwargs=dict(
+        project='tucker_wn18rr',
+        entity = 'aruzzi'
+     ),
+)
+pipeline_result.save_to_directory('results/wn18rr_tucker')
