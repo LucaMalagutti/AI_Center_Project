f0605baee0b4a57dc3c4df2345775cf7e832c5be
Antonio Lopardo <antonio.lopardo@outlook.com>
2022-05-07 18:23:35 +0200

    add num_epochs and vectors_dir args, fix fasttext loading

--- train.py
+++ train.py
@@ -18,9 +18,12 @@ import dotenv
 # Load environment variables from `.env`.
 dotenv.load_dotenv(override=True)
 
-def load_configuration(path: Union[str, pathlib.Path, os.PathLike]) -> Mapping[str, Any]:
+
+def load_configuration(
+    path: Union[str, pathlib.Path, os.PathLike]
+) -> Mapping[str, Any]:
     """Load a configuration from a JSON or YAML file."""
-    
+
     path = pathlib.Path(path)
     if path.suffix == ".json":
         with path.open() as file:
@@ -30,26 +33,62 @@ def load_configuration(path: Union[str, pathlib.Path, os.PathLike]) -> Mapping[s
         with path.open() as file:
             return yaml.safe_load(file)
 
-    raise ValueError(f"Unknown configuration file format: {path.suffix}. Valid formats: json, yaml")
+    raise ValueError(
+        f"Unknown configuration file format: {path.suffix}. Valid formats: json, yaml"
+    )
 
-def get_entity_initializer(init:str, embedding_dim, dataset_name):
+
+def get_entity_initializer(
+    init: str, embedding_dim, dataset_name, vectors_dir="word_vectors"
+):
     """Get an Entity embeddings initializer."""
 
-    if init == "w2v":
-        entity_initializer = PretrainedInitializer(w2v.get_emb_matrix(f"word_vectors/cc.en.{embedding_dim}.bin", embedding_dim, sub_word=True, dataset_name=dataset_name.upper()))
+    if init == "fasttext":
+        entity_initializer = PretrainedInitializer(
+            w2v.get_emb_matrix(
+                f"{vectors_dir}/cc.en.{embedding_dim}.bin",
+                embedding_dim,
+                sub_word=True,
+                dataset_name=dataset_name,
+            )
+        )
+    elif init == "w2v":
+        entity_initializer = PretrainedInitializer(
+            w2v.get_emb_matrix(
+                f"{vectors_dir}/word2vec-google-news-{embedding_dim}.bin",
+                embedding_dim,
+                sub_word=False,
+                dataset_name=dataset_name,
+            )
+        )
     elif init == "glove":
-        entity_initializer = PretrainedInitializer(w2v.get_emb_matrix(f"word_vectors/glove-wiki-gigaword-{embedding_dim}.bin", embedding_dim, sub_word=False, dataset_name=dataset_name.upper()))
+        entity_initializer = PretrainedInitializer(
+            w2v.get_emb_matrix(
+                f"{vectors_dir}/glove-wiki-gigaword-{embedding_dim}.bin",
+                embedding_dim,
+                sub_word=False,
+                dataset_name=dataset_name,
+            )
+        )
     elif init == "bert":
-        with open('word_vectors/bert-mini_no_def.pickle', 'rb') as f:
+        with open("{vectors_dir}/bert-mini_no_def.pickle", "rb") as f:
             bert_emb_matrix = pickle.load(f)
-        print(bert_emb_matrix.shape) # prints torch.Size([40559, 256])
-        
+        print(bert_emb_matrix.shape)  # prints torch.Size([40559, 256])
+
         entity_initializer = PretrainedInitializer(bert_emb_matrix)
     else:
         entity_initializer = "xavier_normal"
     return entity_initializer
 
-def pipeline_from_config(dataset_name: str,model_name :str,init: str, embdim : int):
+
+def pipeline_from_config(
+    dataset_name: str,
+    model_name: str,
+    init: str,
+    embdim: int,
+    epochs: int,
+    vectors_dir: str,
+):
     """Initialize pipeline parameters from config file."""
 
     config_path = f"./config/{model_name.lower()}_{dataset_name.lower()}.json"
@@ -60,39 +99,83 @@ def pipeline_from_config(dataset_name: str,model_name :str,init: str, embdim : i
     else:
         embedding_dim = embdim
 
-    entity_initializer = get_entity_initializer(init, embedding_dim, dataset_name)
+    if epochs is None:
+        num_epochs = config["pipeline"]["training_kwargs"]["num_epochs"]
+    else:
+        num_epochs = epochs
+
+    entity_initializer = get_entity_initializer(
+        init, embedding_dim, dataset_name, vectors_dir
+    )
 
     config["pipeline"]["model_kwargs"]["entity_initializer"] = entity_initializer
     config["pipeline"]["model_kwargs"]["embedding_dim"] = embedding_dim
+    config["pipeline"]["training_kwargs"]["num_epochs"] = num_epochs
 
     run_name = f"{init}_{embedding_dim}_{model_name}_{dataset_name}"
 
     pipeline_kwargs = config["pipeline"]
     pipeline_result = pipeline(
-        metadata= dict(
-        title = run_name,
+        metadata=dict(
+            title=run_name,
         ),
-        result_tracker='wandb',
+        result_tracker="wandb",
         result_tracker_kwargs=dict(
-            project='W2V_for_KGs',
-            entity = 'eth_ai_center_kg_project',
+            project="W2V_for_KGs",
+            entity="eth_ai_center_kg_project",
         ),
-        **pipeline_kwargs
+        **pipeline_kwargs,
     )
 
+    pipeline_result.save_to_directory(f"results/{run_name}")
 
-    pipeline_result.save_to_directory(f'results/{run_name}')
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="wn18rr", nargs="?",
-                    help="Which dataset to use: fb15k237 or wn18rr.")
-    parser.add_argument("--model", type=str, default="tucker", nargs="?",
-                    help="Whihc model to train: tucker")
-    parser.add_argument("--init", type=str, default="baseline", nargs="?",
-                    help="How to initialise embeddings: baseline, w2v, glove, bert")
-    parser.add_argument("--embdim", type=int, default=None, nargs="?",
-                    help="Dimension of embedding vectors, if None then embedding_dim in the .json file will be used")
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="wn18rr",
+        nargs="?",
+        help="Which dataset to use: fb15k237 or wn18rr.",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="tucker",
+        nargs="?",
+        help="Whihc model to train: tucker",
+    )
+    parser.add_argument(
+        "--init",
+        type=str,
+        default="baseline",
+        nargs="?",
+        help="How to initialise embeddings: baseline, w2v, fasttext, glove, bert",
+    )
+    parser.add_argument(
+        "--embdim",
+        type=int,
+        default=None,
+        nargs="?",
+        help="Dimension of embedding vectors, if None then embedding_dim in the .json file will be used",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=None,
+        nargs="?",
+        help="Number of epochs to train the model for",
+    )
+    parser.add_argument(
+        "--vectors_dir",
+        type=str,
+        default=None,
+        nargs="?",
+        help="Directory where vectors are stored",
+    )
 
     args = parser.parse_args()
-    pipeline_from_config(args.dataset,args.model,args.init,args.embdim)
+    pipeline_from_config(
+        args.dataset, args.model, args.init, args.embdim, args.epochs, args.vectors_dir
+    )
