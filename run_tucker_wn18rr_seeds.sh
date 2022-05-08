ff0423a7b8e6366358412574fa98fb489d2f1145
Antonio Lopardo <antonio.lopardo@outlook.com>
2022-05-09 00:01:51 +0200

    add random seed arg in running script

--- run_tucker_wn18rr_seeds.sh
+++ run_tucker_wn18rr_seeds.sh
@@ -7,7 +7,7 @@ n_seeds=$1
 emb=$2
 for (( c=1; c<= n_seeds; c++ ))
 do  
-   cmd="python train.py --dataset=wn18rr --model=tucker --init=${emb} --embdim=200 --epochs=250 --wandb_group=std_runs_${emb}"
+   cmd="python train.py --dataset=wn18rr --model=tucker --init=${emb} --embdim=200 --epochs=250 --wandb_group=std_runs_${emb} --random_seed=${c}" 
    bsub  -n 16 -W 11:59 -R "rusage[mem=5000,ngpus_excl_p=1]" -R "select[gpu_mtotal0>=16000]" ${cmd}
    sleep 1
 done
