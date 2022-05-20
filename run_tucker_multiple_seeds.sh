7adfb201fe919e1243cf012f414e393fa02a828c
pierobartolo <piersilvio.debartolomeis@mail.polimi.it>
2022-05-20 23:00:51 +0200

    add group name to run_tucker_multiple_seeds

--- run_tucker_multiple_seeds.sh
+++ run_tucker_multiple_seeds.sh
@@ -9,7 +9,7 @@ embdim=$3
 data=$4
 for (( c=1; c<= n_seeds; c++ ))
 do  
-   cmd="python train.py --dataset=${data} --random_seed=${c} --model=tucker --init=${emb} --embdim=${embdim} --epochs=500 --wandb_group=std_runs_${emb}_${embdim}"
+   cmd="python train.py --dataset=${data} --random_seed=${c} --model=tucker --init=${emb} --embdim=${embdim} --epochs=500 --wandb_group=std_runs_${emb}_${embdim}_${data}"
    bsub  -n 16 -W 11:59 -R "rusage[mem=5000,ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10000]" ${cmd}
    sleep 1
 done
