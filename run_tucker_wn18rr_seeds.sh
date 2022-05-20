69715734295f8485f3d63f820f5d98fae415c241
pierobartolo <piersilvio.debartolomeis@mail.polimi.it>
2022-05-20 22:22:57 +0200

    add dropout for FB, add FB to run_seeds script

--- run_tucker_wn18rr_seeds.sh
+++ run_tucker_wn18rr_seeds.sh
@@ -6,9 +6,10 @@
 n_seeds=$1
 emb=$2
 embdim=$3
+data=$4
 for (( c=1; c<= n_seeds; c++ ))
 do  
-   cmd="python train.py --dataset=wn18rr --random_seed=${c} --model=tucker --init=${emb} --embdim=${embdim} --epochs=500 --wandb_group=std_runs_${emb}_${embdim}"
-   bsub  -n 16 -W 11:59 -R "rusage[mem=5000,ngpus_excl_p=1]" -R "select[gpu_mtotal0>=20000]" ${cmd}
+   cmd="python train.py --dataset=${data} --random_seed=${c} --model=tucker --init=${emb} --embdim=${embdim} --epochs=500 --wandb_group=std_runs_${emb}_${embdim}"
+   bsub  -n 16 -W 11:59 -R "rusage[mem=5000,ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10000]" ${cmd}
    sleep 1
 done
