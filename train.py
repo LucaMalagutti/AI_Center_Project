8f126c1ca43c240dc46bc0fab5faded48417b2dc
LucaMalagutti <malagutti.luca@yahoo.it>
2022-06-25 12:54:14 +0200

    Removed unsupported relation_dim from distmult pipeline

--- train.py
+++ train.py
@@ -178,7 +178,7 @@ def pipeline_from_config(
 
     config["pipeline"]["model_kwargs"]["entity_initializer"] = entity_initializer
     config["pipeline"]["model_kwargs"]["relation_initializer"] = relation_initializer
-    if relation_init and model_name != "mure":
+    if relation_init and model_name not in ["mure", "distmult"]:
         config["pipeline"]["model_kwargs"]["relation_dim"] = embedding_dim
     config["pipeline"]["model_kwargs"]["embedding_dim"] = embedding_dim
     config["pipeline"]["training_kwargs"]["num_epochs"] = num_epochs
